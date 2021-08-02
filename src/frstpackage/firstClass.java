cfb61e5a0bf6ade39422ede40f9f64e541a504aa
ferhatmanduz <68313191+ferhatmanduz@users.noreply.github.com>
2021-08-02 01:43:37 -0400

    third commit from remote master branch

--- src/frstpackage/firstClass.java
+++ src/frstpackage/firstClass.java
@@ -9,6 +9,7 @@ public class firstClass {
 		System.out.println("third commit from local master");
 		System.out.println("first commit from remote master branch");
 		System.out.println("second commit from remote master branch");
+		System.out.println("third commit from remote master branch");
 	}
 
 }
